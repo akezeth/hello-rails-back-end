506dbf4e10461db3915b6cc98674d722b9ccc711
akezeth <akezeth@gmail.com>
2023-11-11 23:36:08 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      get 'greetings/index'
+      resources :greetings, only: [:index]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
